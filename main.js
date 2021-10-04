ad707c6d03d0aa52f9d78544944f04d2bdf32257
Kyaw Min Lwin <kyawminlwinprogrammer@gmail.com>
2021-10-04 22:17:18 +0630

    m

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ async function lookWeather() {
     let lat = data.coord.lat, lon = data.coord.lon;
     let result2 = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=4a56c3d5b17cbc8e082c506721b008aa&units=metric`);
     let data2 = await result2.json();
-    console.log(data, data2, new Date(data2.hourly[0].dt), new Date(data2.hourly[1].dt))
+    console.log(data2.hourly[21]);
+    let date = new Date('December 17, 1995 21:00:00');
+    console.log(date.getHours())
     // let weatherPic = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     // console.log(weatherPic)
     // console.log(`http://openweathermap.org/img/wn/01d.png`)
 }
 
 lookWeather()
-let count = 0
-for (let i = 1; i <= 100; i += 2) {
-    count++;
-}
-console.log(count)
